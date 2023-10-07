eeb95597a93c7a72f5727470eadbb9224d254ac6
juliensale <sale.julien@protonmail.com>
2023-10-07 14:18:42 +0300

    feat: layout component

---
+++ components/Layout/index.tsx
@@ -0,0 +1,23 @@
+import { FC, PropsWithChildren } from 'react';
+import CrossClose from '../icons/CrossClose';
+import styles from './Layout.module.css';
+import Navbar from './Navbar';
+
+const Layout: FC<PropsWithChildren> = ({ children }) => {
+	return (
+		<div className={styles.container}>
+			<header className={styles.header}>
+				<h1>CEIC Data Bridge</h1>
+				<CrossClose />
+			</header>
+			<div className={styles.navContainer}>
+				<Navbar />
+				<div>
+					{children}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Layout
