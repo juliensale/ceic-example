17a6bbcdaba3ca7df540a7b229cdbe27b0716523
juliensale <sale.julien@protonmail.com>
2023-10-07 14:15:41 +0300

    feat(icon): cross close

---
+++ components/icons/CrossClose.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+
+const CrossClose: FC = () => {
+	return (
+		<svg width="12" height="12" viewBox="0 0 12 12" fill="inherit" xmlns="http://www.w3.org/2000/svg">
+			<path d="M10.9215 11.9727L6.01366 7.06758L1.0785 12L0 10.9221L4.93516 5.98967L0.0206653 1.0779L1.09917 0L6.01366 4.91177L10.9013 0.0268604L11.9798 1.10476L7.09216 5.98967L12 10.8948L10.9215 11.9727Z" fill="inherit" />
+		</svg>
+
+	)
+}
+
+export default CrossClose
